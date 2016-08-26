6fc17434af7c07aabb88e0f16333bdc56371cce8
steve reaume <steve.reaume@metatecture.ca>
2016-08-26 10:10:04 -0400

    11
    11

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -7,4 +7,11 @@
  * Code distributed by Google as part of the polymer project is also
  * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
  */
-console.info('Service worker disabled for development, will be generated at build time.');
+module.exports = {
+  staticFileGlobs: [
+    '/index.html',
+    '/manifest.json',
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
+  ],
+  navigateFallback: '/index.html'
+};
